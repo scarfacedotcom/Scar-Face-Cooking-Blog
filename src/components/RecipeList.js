5104221a1ec712e5457b2121a3d1fbc06e6b5feb
scarfacedotcom <peteradaaku@gmail.com>
2022-06-22 22:04:49 +0100

    added the code for recipe component

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,3 +1,6 @@
+import { NavLink } from 'react-router-dom'
+
+//styles
 import './RecipeList.css'
 
 
@@ -6,7 +9,10 @@ export default function RecipeList({ recipes }) {
     <div className='recipe-list' >
       {recipes.map(recipe => (
         <div key={recipe.id} className="card" >
-          <h3></h3>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.cookingTime} to make</p>
+          <div>{recipe.method.substring(0,100)}...</div>
+          <NavLink to={`/recipes/${recipe.id}`}>Cook this</NavLink>
         </div>
       ))}
     </div>
