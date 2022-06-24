d2d5c984a0b6bb45e9aa17b7894354f309d75030
scarfacedotcom <peteradaaku@gmail.com>
2022-06-24 22:14:32 +0100

    added the styling for create component

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -9,10 +9,47 @@ export default function Create() {
   const [ method, setMethod ] = useState('')
   const [ cookingTime, setCookingTime ] = useState('')
 
+  const handleSubmit =(e) => {
+    e.preventDefault()
+    console.log(title, method, cookingTime)
+  }
+
   return (
     <div className='create'>
       <h2 className="page-title">Add a new Recipe</h2>
-      
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Recipe Title</span>
+          <input 
+            type="text" 
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Recipe Method</span>
+          <textarea 
+            onChange={(e) => setMethod(e.target.value)}
+            value={method}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Cooking time (minutes) </span>
+          <input 
+            type="number" 
+            onChange={(e) => setCookingTime(e.target.value)}
+            value={cookingTime}
+            required
+          />
+        </label>
+
+        <button className="btn">Submit</button>
+      </form>
+
     </div>
   )
 }
