19f456b650a960be491b059e03afb9f4e83356d4
scarfacedotcom <peteradaaku@gmail.com>
2022-07-04 16:22:21 +0100

    added the query params code

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,7 +1,13 @@
+import { useLocation } from 'react-router-dom'
+
 // styles
 import './Search.css'
 
 export default function Search() {
+  const queryString = useLocation()
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('a')
+
   return (
     <div>
       Search
