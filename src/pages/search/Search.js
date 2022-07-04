7a4a035322c6e9796c0d8b5771cb58d00f408eed
scarfacedotcom <peteradaaku@gmail.com>
2022-07-04 16:52:03 +0100

    few changes

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -6,9 +6,9 @@ import RecipeList from '../../components/RecipeList'
 import './Search.css'
 
 export default function Search() {
-  const queryString = useLocation()
+  const queryString = useLocation().search
   const queryParams = new URLSearchParams(queryString)
-  const query = queryParams.get('a')
+  const query = queryParams.get('q')
 
   const url = 'http://localhost:3000/recipes?q=' + query
   const { error, isPending, data } = useFetch(url)
