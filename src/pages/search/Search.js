fbeba860649e71ecf5b1f4821b5be37fc47cdbb1
scarfacedotcom <peteradaaku@gmail.com>
2022-07-04 16:40:31 +0100

    few changes

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,4 +1,6 @@
 import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 
 // styles
 import './Search.css'
@@ -8,9 +10,15 @@ export default function Search() {
   const queryParams = new URLSearchParams(queryString)
   const query = queryParams.get('a')
 
+  const url = 'http://localhost:3000/recipes?q=' + query
+  const { error, isPending, data } = useFetch(url)
+
   return (
     <div>
-      Search
+      <h2 className="page-title">Recipes including "{query}</h2>
+      {error && <p className="error">{error}</p> }
+      {isPending && <p className='loading'>Loading...</p> }
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
