92bbd609856c4b3d734794be30e92aeae76227d2
scarfacedotcom <peteradaaku@gmail.com>
2022-07-05 06:00:13 +0100

    added code to access the content value in the mav bar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,16 @@
-import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 
-import React from 'react'
+import React, { useContext } from 'react'
 import Searchbar from './Searchbar'
+import { ThemeContext } from '../context/ThemeContext'
+import { NavLink } from 'react-router-dom'
+
 
 export default function Navbar() {
+  const { color } = useContext(ThemeContext)
+
   return (
-    <div className='navbar'>
+    <div className='navbar'style={{ background: color }}>
       <nav>
         <NavLink to="/" className="brand">Cook with Scar Face</NavLink>
         <Searchbar />
