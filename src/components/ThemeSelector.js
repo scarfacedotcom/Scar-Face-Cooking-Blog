da017866b9426c27cc8a60a59acae19b1c15312e
scarfacedotcom <peteradaaku@gmail.com>
2022-07-06 14:23:44 +0100

    few changes

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,14 +1,28 @@
 import './ThemeSelector.css'
 
 import { useTheme } from '../hooks/useTheme'
+import modeIcon from '../assets/mode-icon.svg'
 
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
 
-  const { changeColor } = useTheme()
+  const { changeColor, changeMode, mode } = useTheme()
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark')
+  }
+
   return (
     <div className='theme-selector'>
+      <div className="mode-toggle">
+        <img
+          onClick={toggleMode} 
+          src={modeIcon} 
+          alt="imagetoggle" 
+          style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)' }}
+        />
+      </div>
       <div className="theme-buttons">
         {themeColors.map(color => (
           <div
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -28,7 +28,7 @@ export function ThemeProvider ({ children }) {
   }
 
   return (
-    <ThemeContext.Provider value={{...state, changeColor, changeMode}}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
     </ThemeContext.Provider>
   )
