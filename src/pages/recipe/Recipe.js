4c8d6c85140f626164016afa98dd8000ec1317bc
scarfacedotcom <peteradaaku@gmail.com>
2022-07-27 06:01:54 +0100

    few changes

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -13,13 +13,13 @@ export default function Recipe() {
 
   const [ recipe, setRecipe ] = useState(null)
   const [ isPending, setIspending ] = useState(false)
-  const [ error, setError ] = useState(false)
+  const [ error, setError ] = useState(null)
 
   useEffect(() => {
 
     setIspending(true)
 
-    projectFireStore.collection('recipes').doc(id).get().then((doc) => {
+    const unsub = projectFireStore.collection('recipes').doc(id).onSnapshot((doc) => {
       if(doc.exists) {
         setIspending(false)
         setRecipe(doc.data())
@@ -29,11 +29,13 @@ export default function Recipe() {
       }
     })
 
+    return () => unsub()
 
-  }, [])
+
+  }, [id])
 
   const handleClick = () => {
-    projectFireStore.collection('recipe').doc(id).update({
+    projectFireStore.collection('recipes').doc(id).update({
       title: 'something different'
     })
   }
@@ -51,9 +53,67 @@ export default function Recipe() {
             {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
           </ul>
           <p className="method">{recipe.method}</p>
-          <button onClick={() => handClick}>Update Me</button>
+          <button onClick={handleClick}>Update Me</button>
         </>
       )}
     </div>
   )
 }
+
+// import { useParams } from 'react-router-dom'
+// import { useTheme } from '../../hooks/useTheme'
+// import { useState, useEffect } from 'react'
+// import { projectFireStore } from '../../firebase/config'
+
+// // styles
+// import './Recipe.css'
+
+// export default function Recipe() {
+//   const { id } = useParams()
+//   const { mode } = useTheme()
+
+//   const [isPending, setIsPending] = useState(false)
+//   const [error, setError] = useState(null)
+//   const [recipe, setRecipe] = useState(null)
+
+//   useEffect(() => {
+//     setIsPending(true)
+
+//     const unsub = projectFireStore.collection('recipes').doc(id).onSnapshot(doc => {
+//       if (doc.exists) {
+//         setIsPending(false)
+//         setRecipe(doc.data())
+//       } else {
+//         setIsPending(false)
+//         setError(`Could not find that recipe`)
+//       }
+//     })
+
+//     return () => unsub()
+
+//   }, [id])
+
+//   const handleClick = () => {
+//     projectFireStore.collection('recipes').doc(id).update({
+//       title: 'Something completely different'
+//     })
+//   }
+
+//   return (
+//     <div className={`recipe ${mode}`}>
+//       {error && <p className="error">{error}</p>}
+//       {isPending && <p className="loading">Loading...</p>}
+//       {recipe && (
+//         <>
+//           <h2 className="page-title">{recipe.title}</h2>
+//           <p>Takes {recipe.cookingTime} to cook.</p>
+//           <ul>
+//             {recipe.ingredients.map(ing => <li key={ing}>ing</li>)}
+//           </ul>
+//           <p className="method">{recipe.method}</p>
+//           <button onClick={handleClick}>Update me</button>
+//         </>
+//       )}
+//     </div>
+//   )
+// }
